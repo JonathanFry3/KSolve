6d14a03ee3d95941b59e2749f3aeba154d330fe3
Jonathan Fry <jonfry@yahoo.com>
2020-10-09 16:43:35 -0400

    should be in previous commit

--- KSolveAStar.hpp
+++ KSolveAStar.hpp
@@ -30,8 +30,8 @@
 
 // Solves the game of Klondike Solitaire for minimum moves if possible.
 // Returns a result code and a Moves vector.  The vector contains
-// the minimum solution if the code returned is SOLVED. It will contain
-// a solution that may not be minimal if the code is GAVEUP_SOLVED.
+// the minimum solution if the code returned is Solved. It will contain
+// a solution that may not be minimal if the code is GaveUpSolved.
 // Otherwise, it will be empty.
 //
 // This function uses an unpredictable amount of main memory. You can
@@ -40,7 +40,7 @@
 //
 // For some insight into how it works, look up the A* algorithm.
 
-enum KSolveAStarCode {SOLVED, GAVEUP_SOLVED, GAVEUP_UNSOLVED, IMPOSSIBLE, MEMORY_EXCEEDED};
+enum KSolveAStarCode {Solved, GaveUpSolved, GaveUpUnsolved, Impossible, MemoryExceeded};
 struct KSolveAStarResult
 {
 	KSolveAStarCode _code;
