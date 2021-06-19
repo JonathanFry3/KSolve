68dae5745daf308b64cc3c9cae1fee6da749fd31
Jonathan Fry <jonfry@yahoo.com>
2021-06-19 17:26:50 -0400

    Initial changes with minimal extension

--- KSolveAStar.cpp
+++ KSolveAStar.cpp
@@ -15,7 +15,7 @@ typedef std::shared_lock<SharedMutex> SharedGuard;
 typedef std::lock_guard<SharedMutex> ExclusiveGuard;
 
 enum {maxMoves = 512};
-typedef fixed_capacity_deque<Move,maxMoves> MoveSequenceType;
+typedef static_deque<Move,maxMoves> MoveSequenceType;
 
 using namespace frystl;
 
--- fixed_capacity_deque.hpp
+++ fixed_capacity_deque.hpp
@@ -1,4 +1,4 @@
-// fixed_capacity_deque.hpp - implements fixed-capacity deque-like template class
+// static_deque.hpp - implements fixed-capacity deque-like template class
 //
 // It implements (part of) the deque interface using a fixed-size array.
 // The first elements added to it are placed in the middle, and it can
@@ -10,80 +10,85 @@
 #include <iterator>
 #include <cassert>
 
-template <typename T, unsigned Capacity> class fixed_capacity_deque
+template <typename value_type, unsigned Capacity> 
+class static_deque
 {
     static constexpr unsigned _trueCap{2*Capacity+1};
-    T* _begin;
-    T* _end;
-    T _elem[_trueCap];
+    value_type* _begin;
+    value_type* _end;
+    value_type _elem[_trueCap];
 public:
-    typedef T value_type;
-    typedef T& reference;
-    typedef const T& const_reference;
-    typedef unsigned size_type;
-    typedef T* iterator;
-    typedef const T* const_iterator;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using size_type = uint32_t;
+    using difference_type = std::ptrdiff_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-    fixed_capacity_deque() noexcept
+    static_deque() noexcept
         : _begin(_elem+Capacity)
         , _end(_elem+Capacity)
         {}
     void clear() noexcept {
-        for (T&elem: *this)
-            elem.~T();      // destruct all elements
+        for (reference elem: *this)
+            elem.~value_type();      // destruct all elements
         _begin = _end = _elem+Capacity;
     }
-    ~fixed_capacity_deque() noexcept{
+    ~static_deque() noexcept{
         clear();
     }
     size_type size() const noexcept{
         return _end-_begin;
     }
     bool empty() const noexcept{
-        return _begin==_end;
+        return _begin == _end;
     }
     template <class ... Args>
     void emplace_front(Args...args) noexcept{
         assert(_elem < _begin);
-        new(_begin-1) T(args...);
+        new(_begin-1) value_type(args...);
         _begin -= 1;
     }
-    void push_front(const T& t) noexcept{
+    void push_front(const_reference  t) noexcept{
         emplace_front(t);
     }
     void pop_front() noexcept{
         assert(_begin<_end);
         _begin += 1;
-        (_begin-1)->~T();  //destruct
+        (_begin-1)->~value_type();  //destruct
     }
     template <class ... Args>
     void emplace_back(Args...args){
         assert(_end<_elem+_trueCap);
-        new(_end) T(args...);
+        new(_end) value_type(args...);
         _end += 1;
     }
-    void push_back(const T& t) noexcept{
+    void push_back(const reference  t) noexcept{
         emplace_back(t);
     }
     void pop_back() noexcept{
         assert(_begin<_end);
-        back().~T();  //destruct
+        back().~value_type();  //destruct
         _end -= 1;
     }
-    T& back() noexcept{
+    reference back() noexcept{
         assert(_begin<_end);
         return *(_end-1);
     }
-    const T& back() const noexcept{
+    const_reference back() const noexcept{
         assert(_begin<_end);
         return *(_end-1);
     }
 
-    T& operator[](size_type index)noexcept{
+    reference operator[](size_type index)noexcept{
         assert(_begin+index<_end);
         return *(_begin+index);
     }
-    const T& operator[](size_type index) const noexcept{
+    const_reference operator[](size_type index) const noexcept{
         assert(_begin+index<_end);
         return *(_begin+index);
     }
@@ -99,20 +104,12 @@ public:
     const_iterator end() const noexcept{
         return _end;
     }
-
-    class const_reverse_iterator: public std::iterator<std::random_access_iterator_tag, T>
-    {
-        const T* _p;
-        explicit const_reverse_iterator(const T* p) noexcept
-            : _p(p) {}
-        friend class fixed_capacity_deque;
-    public:
-        const T& operator*() noexcept                               {return *_p;}
-        const_reverse_iterator& operator++(/*prefix*/) noexcept     {--_p;return *this;}
-        const_reverse_iterator& operator--(/*prefix*/) noexcept     {++_p;return *this;}
-        bool operator==(const const_reverse_iterator& other) const noexcept {return _p==other._p;}
-        bool operator!=(const const_reverse_iterator& other) const noexcept {return _p!=other._p;}
-    };
+    reverse_iterator rbegin() noexcept{
+        return reverse_iterator(_end-1);
+    }
+    reverse_iterator rend() noexcept{
+        return reverse_iterator(_begin-1);
+    }
     const_reverse_iterator crbegin() const noexcept{
         return const_reverse_iterator(_end-1);
     }
