f0618c9a0ef1e301629393d5c66ca179b6b059a0
Jonathan Fry <jonfry@yahoo.com>
2021-12-29 11:11:31 -0500

    Add compare.py

---
+++ tests/compare.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+# print(os.getcwd()) printed .../KSolve
+base = pd.read_csv("tests/base1000.txt", sep="\t")
+test = pd.read_csv("tests/rebase100.txt", sep="\t")
+assert test.shape[0] <= base.shape[0]
+indexes = list(range(test.shape[0]))
+basex = base.loc[indexes]
+#print((test.outcome == 0) & (basex.outcome != 0))
+solvesPlus = filter(lambda i: (basex.outcome[i] != 0) & (test.outcome[i] == 0), indexes)
+print("Solves+:",list(test.row[solvesPlus]))
+solvesMinus = filter(lambda i: (basex.outcome[i] == 0) & (test.outcome[i] != 0), indexes)
+print("Solves-:",list(test.row[solvesMinus]))
+movesPlus = filter(lambda i: \
+    ((basex.outcome[i] == test.outcome[i]) & (basex.moves[i] < test.moves[i])), indexes)
+print(" Moves+:",list(test.row[movesPlus]))
+movesMinus = filter(lambda i: \
+    ((basex.outcome[i] == test.outcome[i]) & (basex.moves[i] > test.moves[i])), indexes)
+print(" Moves-:",list(test.row[movesMinus]))
+timePlus = filter(lambda i:  basex.time[i] < test.time[i], indexes)
+print("  Time+:",len(list(test.row[timePlus])))
+timeMinus = filter(lambda i:  basex.time[i] > test.time[i], indexes)
+print("  Time-:",len(list(test.row[timeMinus])))
+form = "{:12.2f}"
+print("Base time:", form.format(sum(basex.time)))
+print("Test time:", form.format(sum(test.time)))
+print ("Base states:", sum(basex.states))
+print ("Test states:", sum(test.states))
