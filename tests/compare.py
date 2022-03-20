422c17cf6935dadb6e2fbc75da4647971563f499
Jonathan Fry <jonfry@yahoo.com>
2022-03-20 13:21:31 -0400

    Fix some non-idiomatic Python

--- tests/compare.py
+++ tests/compare.py
@@ -6,16 +6,16 @@ test = pd.read_csv("/home/jon/prj/KSolve/tests/test1000.txt", sep="\t")
 assert test.shape[0] <= base.shape[0]
 indexes = list(range(test.shape[0]))
 basex = base.loc[indexes]
-#print((test.outcome == 0) & (basex.outcome != 0))
-solvesPlus = filter(lambda i: (basex.outcome[i] != 0) & (test.outcome[i] == 0), indexes)
+#print((test.outcome == 0) and (basex.outcome != 0))
+solvesPlus = filter(lambda i: (basex.outcome[i] != 0) and (test.outcome[i] == 0), indexes)
 print("Solves+:",list(test.row[solvesPlus]))
-solvesMinus = filter(lambda i: (basex.outcome[i] == 0) & (test.outcome[i] != 0), indexes)
+solvesMinus = filter(lambda i: (basex.outcome[i] == 0) and (test.outcome[i] != 0), indexes)
 print("Solves-:",list(test.row[solvesMinus]))
 movesPlus = filter(lambda i: \
-    ((basex.outcome[i] == test.outcome[i]) & (basex.moves[i] < test.moves[i])), indexes)
+    ((basex.outcome[i] == test.outcome[i]) and (basex.moves[i] < test.moves[i])), indexes)
 print(" Moves+:",list(test.row[movesPlus]))
 movesMinus = filter(lambda i: \
-    ((basex.outcome[i] == test.outcome[i]) & (basex.moves[i] > test.moves[i])), indexes)
+    ((basex.outcome[i] == test.outcome[i]) and (basex.moves[i] > test.moves[i])), indexes)
 print(" Moves-:",list(test.row[movesMinus]))
 timePlus = filter(lambda i:  basex.time[i] < test.time[i], indexes)
 print("  Time+:",len(list(test.row[timePlus])))
