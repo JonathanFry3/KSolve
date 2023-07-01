9f732a165383c30b1f3fbac18b518c4a8b1b17ef
Jonathan Fry <jonfry@yahoo.com>
2023-06-30 20:35:31 -0400

    Recrea base files using 16 threads

---
+++ tests/bigtest3.sh
@@ -0,0 +1,1 @@
+../ran --seed 29347 --incr 2349 --draw 3 --end $1 --mvlimit 110000000 
---
+++ tests/new-time-tests.sh
@@ -0,0 +1,5 @@
+./rantest3.sh 100 > test3-100.txt
+./rantest.sh 1000 > test1000.txt
+./bigtest.sh 2000 > test2000-110M-moves.txt
+./bigtest3.sh 2000 > test3-2000-110M-moves.txt
+./rantest.sh 10000 > test10000.txt
