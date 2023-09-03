571cd0e90bb112f7df7ed52ba335f04fe624027e
Jonathan Fry <jonfry@yahoo.com>
2023-09-03 16:00:27 -0400

    4/5 new base result files

--- tests/new-time-tests.sh
+++ tests/new-time-tests.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/sh
 ./rantest3.sh 100 > test3-100.txt
 ./rantest.sh 1000 > test1000.txt
-./bigtest.sh 2000 > test2000-110M-moves.txt
-./bigtest3.sh 2000 > test3-2000-110M-moves.txt
+./bigtest.sh 2000 > bigtest2000.txt
+./bigtest3.sh 2000 > bigtest3-2000.txt
 ./rantest.sh 10000 > test10000.txt
