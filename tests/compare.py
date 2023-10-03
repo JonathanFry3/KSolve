32f9da6f0bd7f1f706c4081251b267c9fcf2fbf2
Jonathan Fry <jonfry@yahoo.com>
2023-10-03 17:19:52 -0400

    Automate processing of time tests.

--- tests/compare.py
+++ tests/compare.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import os
-# print(os.getcwd()) printed .../KSolve
-base = pd.read_csv("tests/base10000.txt", sep="\t")
-test = pd.read_csv("tests/base1000.txt", sep="\t")
+import sys
+print("Comparing", sys.argv[1], "with", sys.argv[2])
+base = pd.read_csv(sys.argv[1], sep="\t")
+test = pd.read_csv(sys.argv[2], sep="\t")
 assert test.shape[0] <= base.shape[0]
 indexes = list(range(test.shape[0]))
 basex = base.loc[indexes]
-#print((test.outcome == 0) and (basex.outcome != 0))
 solvesPlus = basex["outcome"].isin([2,3,4]) & test["outcome"].isin([0,1])
-print("Solves+:",list(test.row[solvesPlus]))
+print(" Solves+:",list(test.row[solvesPlus]))
 solvesMinus = basex["outcome"].isin([0,1]) & test["outcome"].isin([2,3,4])
-print("Solves-:",list(test.row[solvesMinus]))
+print(" Solves-:",list(test.row[solvesMinus]))
 imposPlusGood = filter(lambda i: (basex.outcome[i] == 3) and (test.outcome[i] == 2), indexes)
 print("Imposs++:",list(test.row[imposPlusGood]))
 imposPlusBad = filter(lambda i: (basex.outcome[i] <= 1) and (test.outcome[i] == 2), indexes)
 print("Imposs+-:",list(test.row[imposPlusBad]))
 imposMinus = filter(lambda i: (basex.outcome[i] == 2) and (test.outcome[i] != 2), indexes)
-print("Imposs-:",list(test.row[imposMinus]))
+print(" Imposs-:",list(test.row[imposMinus]))
 movesPlus = basex["outcome"].isin([0,1]) & test["outcome"].isin([0,1]) \
         & (test["moves"] > basex["moves"])
-print(" Moves+:",list(test.row[movesPlus]))
+print("  Moves+:",list(test.row[movesPlus]))
 movesMinus = basex["outcome"].isin([0,1]) & test["outcome"].isin([0,1]) \
         & (test["moves"] < basex["moves"])
-print(" Moves-:",list(test.row[movesMinus]))
+print("  Moves-:",list(test.row[movesMinus]))
 timePlus = filter(lambda i:  basex.time[i] < test.time[i], indexes)
-print("  Time+:",len(list(test.row[timePlus])))
+print("   Time+:",len(list(test.row[timePlus])))
 timeMinus = filter(lambda i:  basex.time[i] > test.time[i], indexes)
-print("  Time-:",len(list(test.row[timeMinus])))
+print("   Time-:",len(list(test.row[timeMinus])))
 form = "{:12.2f}"
 pctForm = "{:+.2f}%"
 baseTot = sum(basex.time)
