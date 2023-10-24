6731c45f00bff75ea4b3db3b250639e0fb07ecf5
Jonathan Fry <jonfry@yahoo.com>
2023-10-24 12:39:37 -0400

    Upgrade large-memory test scripts

--- tests/bigtest.sh
+++ tests/bigtest.sh
@@ -1,2 +1,2 @@
-../ran --seed 85723 --end 1 --mvlimit 150000000 > /dev/null # prime system
+../ran --seed 3024322 --end 1 --mvlimit 150000000 > /dev/null # prime system
 ../ran --seed 29347 --incr 2349 --end $1 --mvlimit 150000000 
