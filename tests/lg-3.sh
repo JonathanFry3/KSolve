54288c5004f8ed0675f0cecce3c59c6e9747e7e3
Jonathan Fry <jonfry@yahoo.com>
2024-05-04 21:41:52 -0400

    New, and very different, benchmark results

--- tests/lg-3.sh
+++ tests/lg-3.sh
@@ -1,2 +1,2 @@
-../ran --seed 3024322 --end 1 --draw 3 --mvlimit 150000000 > /dev/null # prime system
-../ran --seed 29345 --incr 2349 --draw 3 --end $1 --mvlimit 150000000 
+../ran --seed 3024322 --end 1 --draw 3 --mvlimit 50000000 > /dev/null # prime system
+../ran --seed 29345 --incr 2349 --draw 3 --end $1 --mvlimit 50000000 
--- tests/sm-1.sh
+++ tests/sm-1.sh
@@ -1,2 +1,2 @@
-../ran --seed 3024322 --end 1 --draw 3 --mvlimit 30000000 > /dev/null # prime system
-../ran --seed 29345 --incr 2349 --end $1 --mvlimit 30000000 
+../ran --seed 3024322 --end 1 --draw 3 --mvlimit 10000000 > /dev/null # prime system
+../ran --seed 29345 --incr 2349 --end $1 --mvlimit 10000000 
