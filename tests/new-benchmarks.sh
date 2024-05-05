e6ea6603f0c69c5aad3b50dccb496d191b760d03
Jonathan Fry <jonfry@yahoo.com>
2024-05-05 13:00:00 -0400

    Correct bad checkin of new-benchmarks.sh

--- tests/new-benchmarks.sh
+++ tests/new-benchmarks.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 ./lg-1.sh 1000 > lg-1-test-1000.txt
 ./lg-3.sh 1000 > lg-3-test-1000.txt
-#./sm-1.sh 1000 > sm-1-test-1000.txt
-#./sm-3.sh 1000 > sm-3-test-1000.txt
+./sm-1.sh 1000 > sm-1-test-1000.txt
+./sm-3.sh 1000 > sm-3-test-1000.txt
