93fc35fb142a830fe2f0b1b56651ccc8adfd7789
Jonathan Fry <jonfry@yahoo.com>
2024-05-06 16:59:08 -0400

    Choose a harder problem to prime system in tests

--- tests/sm-3.sh
+++ tests/sm-3.sh
@@ -1,3 +1,3 @@
-../ran --seed 3024322 --end 1 --draw 3 --mvlimit 10000000 > /dev/null # prime system
+../ran --seed 1633712 --end 1 --draw 3 --mvlimit 10000000 > /dev/null # prime system
 ../ran --seed 29345 --incr 2349 --end $1 --draw 3 --mvlimit 10000000 > sm-3-test-$1.txt\
 && python3 compare.py sm-3-base-1000.txt sm-3-test-$1.txt
