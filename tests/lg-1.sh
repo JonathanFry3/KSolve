5d604e44963bf8b99b21c85a716b82fc33eab95d
Jonathan Fry <jonfry@yahoo.com>
2024-06-13 14:09:46 -0400

    Revert large memory test scripts to use 50 million move limits.

--- tests/lg-1.sh
+++ tests/lg-1.sh
@@ -1,3 +1,3 @@
-../ran --seed 1509215 --end 1 --mvlimit 200000000 > /dev/null # prime system
-../ran --seed 29345 --incr 2349 --end $1 --mvlimit 200000000 > lg-1-test-$1.txt\
+../ran --seed 1509215 --end 1 --mvlimit 50000000 > /dev/null # prime system
+../ran --seed 29345 --incr 2349 --end $1 --mvlimit 50000000 > lg-1-test-$1.txt\
 && python3 compare.py lg-1-base-1000.txt lg-1-test-$1.txt
