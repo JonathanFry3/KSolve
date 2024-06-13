3259ea4a7ddf23f4e43bd6e4b013022efbb26af9
Jonathan Fry <jonfry@yahoo.com>
2024-06-13 11:07:19 -0400

    Drop small-mmemory tests from standard benchmarks

--- tests/check-benchmarks.sh
+++ tests/check-benchmarks.sh
@@ -1,4 +1,2 @@
 python3 compare.py lg-1-base-1000.txt lg-1-test-1000.txt
 python3 compare.py lg-3-base-1000.txt lg-3-test-1000.txt
-python3 compare.py sm-1-base-1000.txt sm-1-test-1000.txt
-python3 compare.py sm-3-base-1000.txt sm-3-test-1000.txt
--- tests/promote-benchmarks.sh
+++ tests/promote-benchmarks.sh
@@ -1,4 +1,2 @@
 mv -f lg-1-test-1000.txt lg-1-base-1000.txt
 mv -f lg-3-test-1000.txt lg-3-base-1000.txt
-mv -f sm-1-test-1000.txt sm-1-base-1000.txt
-mv -f sm-3-test-1000.txt sm-3-base-1000.txt
